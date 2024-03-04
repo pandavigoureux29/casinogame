19f013e5462332e91aeb5ff7ee8977e2a2e00663
pandavigoureux29 <sauceur2chiffre>
2024-03-04 12:17:38 -0500

    first commit

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+using Photon.Realtime;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+
+	public PhotonView playerPrefab;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to Master");
+		PhotonNetwork.JoinRandomOrCreateRoom();
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("Joined a room.");
+		PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+	}
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.Log("Other Player Joined this room.");
+    }
+
+}
